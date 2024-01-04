4740c7566dc81f1cc350a38a59863fee7f724824
Aidan <ajberger905@gmail.com>
2024-01-03 21:24:33 -0500

    uri

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
-const API_BASE = 'https://journova-stg-dbff357caad1.herokuapp.com'
+const API_BASE = 'https://journova-production-fddb909876c2.herokuapp.com'
 const API_BASE_1 = 'http://localhost:3001'
 
 export default API_BASE;

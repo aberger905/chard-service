c8aa10c310be6ead586d84cf49cafa4d39889691
Aidan <ajberger905@gmail.com>
2024-01-07 17:25:26 -0500

    nav change

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
                     {/* Primary Navbar items */}
                     <div className="hidden md:flex items-center space-x-1">
                         <Link to='http://journova.org' className="py-4 text-white px-2 hover:text-slate-300 transition duration-300">Home</Link>
-                        <Link to='/' className="py-4 text-white px-2 hover:text-slate-300 transition duration-300">Create</Link>
+                        <Link to='/' className="py-4 text-white px-2 hover:text-slate-300 transition duration-300">Get Started</Link>
                         <Link to='/pricing' className="py-4 text-white px-2 hover:text-slate-300 transition duration-300">Pricing</Link>
                         <Link to='/about' className="py-4 text-white px-2 hover:text-slate-300 transition duration-300">About</Link>
                     </div>
@@ -36,7 +36,7 @@ const Navbar = () => {
             {/* Mobile Menu */}
             <div className={`md:hidden bg-black ${isMenuOpen ? 'block' : 'hidden'}`}>
                 <Link to='http://journova.org' onClick={() => setIsMenuOpen(false)} className="block py-2 px-4 text-sm text-white hover:bg-gray-500">Home</Link>
-                <Link to='/' onClick={() => setIsMenuOpen(false)} className="block py-2 px-4 text-sm text-white hover:bg-gray-500">Create</Link>
+                <Link to='/' onClick={() => setIsMenuOpen(false)} className="block py-2 px-4 text-sm text-white hover:bg-gray-500">Get Started</Link>
                 <Link to='/pricing' onClick={() => setIsMenuOpen(false)} className="block py-2 px-4 text-white text-sm hover:bg-gray-500">Pricing</Link>
                 <Link to='/about' onClick={() => setIsMenuOpen(false)} className="block py-2 px-4 text-white text-sm hover:bg-gray-500">About</Link>
             </div>

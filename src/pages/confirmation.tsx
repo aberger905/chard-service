fcd74c3490dbaf0f0eef034106c6f27d0ee97c41
Aidan <ajberger905@gmail.com>
2024-01-08 16:27:39 -0500

    confirmation page

--- src/pages/confirmation.tsx
+++ src/pages/confirmation.tsx
@@ -22,7 +22,7 @@ const Confirmation = () => {
       <div className='w-[90%] flex flex-col justify-center items-center mt-5 mb-5'>
       <h1 className='text-3xl sm:text-4xl font-anton mb-5 text-center'>Congratulations on Taking the First Step!</h1>
       <img className='w-[90%] sm:w-96 mt-5 mb-5' src={banner} alt="newspaper graphic" />
-      <p className='text-center text-xl font-anton'>Thank you for your submission! Our team of skilled journalists is ready to bring your story to life. We're excited to start working on your article.</p>
+      <p className='text-center text-xl font-anton'>Thank you for your submission! Our team of skilled journalists is ready to bring your story to life.</p>
       <br />
       {/* <p className="text-gray-700 mb-3 text-lg">Here's what happens next:</p>
                 <ul className="text-gray-600 list-inside list-disc mb-4 text-left text-lg">
@@ -30,7 +30,7 @@ const Confirmation = () => {
                     <li>Review and Approval: Once your article is crafted, you'll have the opportunity to review it.</li>
                     <li>Ready for the World: After your approval, we'll prepare your article for publishing.</li>
                 </ul> */}
-      <p className='text-center text-lg text-gray-600'>We appreciate your contribution to our storytelling journey. Your story is now in the hands of our dedicated team, and we're eager to bring it to life. Keep an eye on your inbox for updates and the big reveal of your completed article! If you don't receive a confirmation email within 5 minutes, kindly check your spam folder or contact us at support@journova.com for assistance.</p>
+      <p className='text-center text-lg text-gray-600'>We appreciate your contribution to our storytelling journey. Your story is now in the hands of our dedicated team. We're excited to start working on your article. If you don't receive a confirmation email within 5 minutes, kindly check your spam folder or contact us at support@journova.com for assistance.</p>
     </div>
     </div>
      {/* <div className="bg-gradient-to-t from-green-200 to white min-h-screen flex flex-col items-center justify-center">

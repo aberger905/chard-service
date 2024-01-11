4faa76b9acde14baf090f219f603a11fd1e0d6d4
Aidan <ajberger905@gmail.com>
2024-01-11 10:24:06 -0500

    vars

--- src/settings.ts
+++ src/settings.ts
@@ -4,4 +4,7 @@ const API_BASE_2 = 'https://journova-staging-656537b3767d.herokuapp.com';
 
 const STRIPE_KEY = 'pk_live_51OMv6cLGU8dofAUrNi0JZJyOJva5WUnNFJ07k3c8zOIflrWzV9eeGKODHcNpEP8G7MSrb7qDPIx104Vl2DJ87nhO00lJkePLan'
 
-export {API_BASE, STRIPE_KEY};
+const items = {API_BASE, STRIPE_KEY};
+
+export default items;
+

49f668604385cdd5ce16a9421118aea76a76f4ba
Aidan <ajberger905@gmail.com>
2024-01-18 21:25:20 -0500

    Payment update

--- src/pages/payment.tsx
+++ src/pages/payment.tsx
@@ -30,14 +30,14 @@ const Payment: React.FC = () => {
   return (
     <>
     <div className=' bg-gradient-to-t from-gray-300 to white min-h-screen'>
-    <h1 className='text-4xl text-center pt-5 pb-8 font-anton fadeUp'>Almost There! Choose a Package to Bring Your Story to Life</h1>
+    <h1 className='text-4xl text-center pt-5 pb-8 font-anton fadeUp'>Almost There! Confirm a Package to Bring Your Story to Life</h1>
     {/* <h2 className='text-xl text-center mb-10 font-anton fadeUp'>Select Your Perfect Package.</h2> */}
     <div className='flex flex-col md:flex-row justify-center items-center pb-8'>
       <div className='bg-white flex flex-col justify-center items-center border shadow-sm rounded-lg p-5 w-72 fadeUp'>
       <p className='text-gray-700'>Basic</p>
         <h2 className='font-anton text-lg mt-2'>Article</h2>
-        <p className='text-gray-700 text-center'>Professionally written article tailored to you</p>
-        <h2 className='font-semibold text-4xl'>$59</h2>
+        <p className='text-gray-700 text-center'>Professionally written article</p>
+        <h2 className='font-semibold text-4xl'>$29</h2>
         <p className='text-gray-700 text-center'>one time payment</p>
         <button onClick={() => handleClick('article')} disabled={isLoading} className='border border-2 border-black  transition rounded-full  hover:bg-black hover:text-white px-5 py-2 w-[90%]'>{isLoading ? <div className='flex justify-center items-center'><FaSpinner className='animate-spin'/></div> : 'Continue'}</button>
         <div className='mt-5 mb-5'>
@@ -71,7 +71,7 @@ const Payment: React.FC = () => {
         <p className='text-blue-600 border border-blue-600 p-1 rounded-lg'>Most Popular</p>
         <h2 className='font-anton text-lg mt-2'>Published Article</h2>
         <p className='text-gray-700 text-center'>Professionally written article published on a news platform</p>
-        <h2 className='font-semibold text-4xl'>$99</h2>
+        <h2 className='font-semibold text-4xl'>$49</h2>
         <p className='text-gray-700 text-center'>one time payment</p>
         <button onClick={() => handleClick('published')} disabled={isLoading} className='border border-black border-2 bg-black text-white transition rounded-full  hover:bg-slate-700 hover:border-slate-700 px-5 py-2 w-[90%]'>{isLoading ? <div className='flex justify-center items-center'><FaSpinner className='animate-spin'/></div> : 'Continue'}</button>
         <div className='mt-5 mb-5'>
@@ -105,7 +105,7 @@ const Payment: React.FC = () => {
         <p className='text-yellow-500'>Premium</p>
         <h2 className='font-anton text-lg mt-2'>Published Article</h2>
         <p className='text-gray-700 text-center'>Expedited 24-hour delivery and 1 revision</p>
-        <h2 className='font-semibold text-4xl'>$149</h2>
+        <h2 className='font-semibold text-4xl'>$79</h2>
         <p className='text-gray-700 text-center'>one time payment</p>
         <button onClick={() => handleClick('premium')} disabled={isLoading} className='border border-black border-2 transition rounded-full  hover:bg-black hover:text-white px-5 py-2 w-[90%]'>{isLoading ? <div className='flex justify-center items-center'><FaSpinner className='animate-spin'/></div> : 'Continue'}</button>
         <div className='mt-5 mb-5'>
--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -15,14 +15,14 @@ const Pricing: React.FC = () => {
   return (
     <>
     <div className=' bg-gradient-to-t from-gray-300 to white min-h-screen'>
-    <h1 className='text-5xl text-center pt-5 font-anton fadeUp'>Your Story Awaits its Spotlight.</h1>
-    <h2 className='text-xl text-center mb-10 font-anton fadeUp'>Unlock the power of your narrative.</h2>
+    <h1 className='text-5xl text-center pt-5 font-anton fadeUp'>Give the Gift of Recognition. </h1>
+    <h2 className='text-xl text-center mb-10 font-anton fadeUp'>Celebrate their achievements and share their journey with the world.</h2>
     <div className='flex flex-col md:flex-row justify-center items-center pb-8'>
       <div className='bg-white flex flex-col justify-center items-center border shadow-sm rounded-lg p-5 w-72 fadeUp'>
         <p className='text-gray-700'>Basic</p>
-        <h2 className='text-lg mt-2 font-anton'>Article</h2>
-        <p className='text-gray-700 text-center'>Professionally written article tailored to you</p>
-        <h2 className='font-semibold text-4xl'>$59</h2>
+        <h2 className='text-lg mt-2 font-anton'>Basic Article</h2>
+        <p className='text-gray-700 text-center'>Professionally written article</p>
+        <h2 className='font-semibold text-4xl'>$29</h2>
         <p className='text-gray-700 text-center'>one time payment</p>
         <button onClick={handleClick} className='border border-2 border-black  transition rounded-full  hover:bg-black hover:text-white px-5 py-2 w-[90%]'>Get Started</button>
         <div className='mt-5 mb-5'>
@@ -56,7 +56,7 @@ const Pricing: React.FC = () => {
         <p className='text-blue-600 border border-blue-600 p-1 rounded-lg'>Most Popular</p>
         <h2 className='text-lg mt-2 font-anton'>Published Article</h2>
         <p className='text-gray-700 text-center'>Professionally written article published on a news platform</p>
-        <h2 className='font-semibold text-4xl'>$99</h2>
+        <h2 className='font-semibold text-4xl'>$49</h2>
         <p className='text-gray-700 text-center'>one time payment</p>
         <button onClick={handleClick} className='border border-2 border-black bg-black text-white transition rounded-full  hover:bg-slate-700 hover:border-slate-700 px-5 py-2 w-[90%]'>Get Started</button>
         <div className='mt-5 mb-5'>
@@ -90,7 +90,7 @@ const Pricing: React.FC = () => {
         <p className='text-yellow-500'>Premium</p>
         <h2 className='font-anton text-lg mt-2'>Published Article</h2>
         <p className='text-gray-700 text-center'>Expedited 24-hour delivery and 1 revision</p>
-        <h2 className='font-semibold text-4xl'>$149</h2>
+        <h2 className='font-semibold text-4xl'>$79</h2>
         <p className='text-gray-700 text-center'>one time payment</p>
         <button onClick={handleClick} className='border border-2 border-black  transition rounded-full  hover:bg-black hover:text-white px-5 py-2 w-[90%]'>Get Started</button>
         <div className='mt-5 mb-5'>
